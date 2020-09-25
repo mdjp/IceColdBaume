0c31bec4eeb0a7235bee12c91fa1ba66cbb172d7
Michelle <kogelmichelle@gmail.com>
2020-09-25 17:52:34 -0400

    fix: Rounds and fixes related to rounds
    Once a player completes hole 10 they are reset to hole with the round increased
    Fixed an issue that you couldn't get to hole 10
    The beam pauses and starts the bonus timer when entering and exiting the reset state

--- Game/src/Autoload/PlayerData.gd
+++ Game/src/Autoload/PlayerData.gd
@@ -5,11 +5,12 @@ extends Node
 signal target_updated
 signal reset_beam
 signal add_ball
-signal end_game
 signal start_game_timer
+signal stop_game_timer
 
 var score setget set_score
 var bonus setget set_bonus
+var set setget set_round
 var target_hole setget set_target
 var max_holes: = 10
 var number_of_balls: = 3 setget set_balls
@@ -24,6 +25,7 @@ func reset(start_hole = 1, ball_count = 3) -> void:
 	number_of_balls = ball_count
 	score = 0
 	bonus = 100 * target_hole
+	set = 1
 
 
 func set_score(value: int) -> void:
@@ -38,12 +40,21 @@ func set_bonus(value: int) -> void:
 #		emit_signal("bonus_updated")
 
 
+func set_round(value: int) -> void:
+	if value > 0:
+		set = value
+
+
 func set_target(value: int) -> void:
-	if value in range(1, max_holes):
-		emit_signal("target_updated", target_hole, value) # old_target, new_target
-		target_hole = value
+	var small_value = value
+	if small_value > max_holes:
+		small_value = small_value % max_holes
+		set += 1
+	
+	if small_value in range(1, max_holes + 1):
+		emit_signal("target_updated", target_hole, small_value) # old_target, new_target
+		target_hole = small_value
 		bonus = 100 * target_hole
-		emit_signal("start_game_timer")
 
 
 func set_balls(value: int) -> void:
@@ -55,7 +66,7 @@ func reset_game():
 	emit_signal("reset_beam") # Once the beam is reset it checks whether a new ball needs to be added
 	if number_of_balls == 0:
 		print("Game ended")
-		emit_signal("end_game")
+		emit_signal("stop_game_timer")
 
 
 func game_started():
--- Game/src/Screens/ScoreScreen.gd
+++ Game/src/Screens/ScoreScreen.gd
@@ -3,10 +3,11 @@ extends Node2D
 
 func _ready():
 	PlayerData.connect("start_game_timer", self, "start_timer")
-	PlayerData.connect("end_game", self, "stop_timer")
+	PlayerData.connect("stop_game_timer", self, "stop_timer")
 
 
 func _process(delta):
+	$Background/MarginContainer/HBoxContainer/leftContainer/statuses/RoundLabel.text = "SET\n" + str(PlayerData.set)
 	$Background/MarginContainer/HBoxContainer/right/Container/ballCount/Balls.text = str(PlayerData.number_of_balls)
 	$Background/MarginContainer/HBoxContainer/right/bonusValue/Container/Bonus.text = str(PlayerData.bonus).pad_zeros(4)
 	$Background/MarginContainer/HBoxContainer/right/scoreValue/Container/Score.text = str(PlayerData.score).pad_zeros(5)
